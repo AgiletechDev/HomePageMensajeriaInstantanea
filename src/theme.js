78c45b63d4c26ebf785f6da863bc5f381c78390f
iska1234 <sadhuisaac1@gmail.com>
2024-02-13 10:05:00 -0500

    Colors added

--- src/theme.js
+++ src/theme.js
@@ -4,15 +4,15 @@ export const theme = createTheme({
     palette: {
       primary: {
         gradient: 'linear-gradient(83deg, rgba(119,55,142,1) 0%, rgba(84,85,154,1) 50%, rgba(46,119,167,1) 100%)',
-        main: '#77378e',
+        main: '#d8222c',
         second:'#2f77a7',
       },
       secondary: {
-        main: '#cf4800',
+        main: '#262626',
         second: '#f6f6f6',
       },
       textDark:{
-        main:'#1d1b20',
+        main:'#030303',
         second:'#79747e',
         blue:'#2e3192'
       },
